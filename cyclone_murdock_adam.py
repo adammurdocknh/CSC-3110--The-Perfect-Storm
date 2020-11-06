38108dddb4db57d220e4eff24068a045a64bc02a
Adam <adammurdocknh@gmail.com>
2020-11-06 00:08:30 -0500

    v1.3

--- cyclone_murdock_adam.py
+++ cyclone_murdock_adam.py
@@ -14,14 +14,12 @@ def printGreeing():
     print("Welcome to our tropical cyclone resource.")
     print("*****************************************")
 
-
 """
 Prints instructions for the user.
 """
 def printInstructions():
     print('Would you like to use kilometers per hour or miles per hour?')
     print('Enter k for km/hr, m for mph or q to quit.')
-
 
 """                
 Checks if the variable input is a positive number.
@@ -37,22 +35,21 @@ def checkValidInput(input):
 
 """
 Converts the variable @kmh into mph.
-Checks if @kmh is a valid input and then returns a float.
 """
 def kilometerToMiles(kmh):
     return kmh * 0.621371
 
 """
 Converts the variable @mph into km/hr.
-Checks if @mph is a valid input and then returns a float.
 """
 def milesToKilometers(mph):
     return mph * 1.61
-                
 
+"""             
+Converts the variable @mph into knots
+"""
 def milesToKnots(mph):
     return mph * 0.868976
-
 
 """
 This function prints the hurricane's strength based on the @mph input.
@@ -100,9 +97,36 @@ def typhoonStrength(kmh):
 """
 This function prints the storm's strength using the beaufort wind scale
 based on the @knots input. Uses an if/elif tree to decide.
-# """
-# def beaufortStrength(knots):
-#     print("On the beaufort wind scale this is a:")
+"""
+def beaufortStrength(knots):
+    print("On the beaufort wind scale this is:")
+    if(knots < 1):
+        print("Calm")
+    elif(knots >= 1 and knots < 4):
+        print("Light air")
+    elif(knots >= 4 and knots < 7):
+        print("A Light Breeze")
+    elif(knots >= 7 and knots < 11):
+        print("A 'Gentle Breeze' registered trademark of Proctor and Gamble ")
+    elif(knots >= 11 and knots < 17):
+        print("A Moderate Breeze")
+    elif(knots >= 17 and knots < 22):
+        print("A Fresh Breeze")
+    elif(knots >= 22 and knots < 28):
+        print("A Strong Breeze")
+    elif(knots >= 28 and knots < 34):
+        print("A Near Gale")
+    elif(knots >= 34 and knots < 41):
+        print("A Gale")
+    elif(knots >= 41 and knots < 48):
+        print("A Strong Gale")
+    elif(knots >= 48 and knots < 56):
+        print("A Storm")
+    elif(knots >= 56 and knots < 64):
+        print("A Violent Storm")
+    elif(knots >= 64):
+        print("A Hurricane")
+    print("***")
     
 """
 Function takes no inputs and returns void. 
@@ -115,15 +139,20 @@ def main():
     while(not finished):
         printInstructions()
         inputVal = input()
-        if (inputVal == "m"):
+        if (inputVal.upper() == "M"):
             print("What is the speed of the hurricane in mph?")
             # Getting user input and casting to float
             inputVal = float(input())
+            # Verifying the float is an acceptable value ie the value
+            # is positive and is not zero.
             if(checkValidInput(inputVal)):
                 print("That is", milesToKilometers(inputVal), "km/hr")
+                print("That is", milesToKnots(inputVal), "knots")
                 hurricaneStrength(inputVal)
-                typhoonStrength(milesToKilometers(inputVal))
-        if (inputVal == "k"):
+                typhoonStrength(milesToKilometers(inputVal))                    
+                beaufortStrength(milesToKnots(inputVal))
+    
+        if (inputVal.upper() == "K"):
             print("What is the speed of the hurricane in km/hour?")
             # Getting user input and casting to float
             inputVal = float(input())
@@ -131,9 +160,12 @@ def main():
             # is positive and is not zero.
             if(checkValidInput(inputVal)):
                 print("That is", kilometerToMiles(inputVal), "mph")
+                print("That is", milesToKnots(kilometerToMiles(inputVal)), "knots")
                 hurricaneStrength(inputVal)
                 typhoonStrength(kilometerToMiles(inputVal))
-        if (inputVal == "q"):
+                beaufortStrength(milesToKnots(kilometerToMiles(inputVal)))
+    
+        if (inputVal.upper() == "Q"):
             print("I hope this to was helpful. May munsell be with you.")
             finished = True
     
